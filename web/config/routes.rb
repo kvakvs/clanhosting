bbeae7b7afae7a8d8e6b40ca432f126d9efbbf08
Dmytro Lytovchenko <kvakvs@yandex.ru>
2014-08-22 19:45:31 +0200

    forem as separate github repo. Clan used as category'

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
-  mount Forem::Engine, :at => '/forums'
+  scope '/:clan_id', :clan_id => 'null' do
+    mount Forem::Engine, :at => '/forums', :as => 'forem'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -9,21 +9,40 @@
 # Force-decorate the User class in case it hasn't been yet. Fixes #495.
 Forem.decorate_user_class!
 
-Forem::Category.create(:name => 'General')
+Forem::Category.create(:id => 1, :name => 'clan-bad1')
+Forem::Category.create(:id => 2, :name => 'clan-good')
+Forem::Category.create(:id => 3, :name => 'clan-bad2')
 
 user = Forem.user_class.first
 unless user.nil?
-  forum = Forem::Forum.find_or_create_by(:category_id => Forem::Category.first.id,
-                               :name => "Default",
-                               :description => "Default forem created by install")
+  forum1 = Forem::Forum.find_or_create_by(
+      :category_id => 1, #Forem::Category.find_by(:name => 'clan-good').id,
+      :name => "Clan", :description => "Clan news, announcements, clan-related topics")
+  forum2 = Forem::Forum.find_or_create_by(
+      :category_id => 1, #Forem::Category.find_by(:name => 'clan-good').id,
+      :name => "Off-topic", :description => "Everything else, daily chit-chat")
+  forum_bad1 = Forem::Forum.find_or_create_by(
+      :category_id => 2, #Forem::Category.find_by(:name => 'clan-bad1').id,
+      :name => "Bad1 Forum", :description => "Shound not see this")
+  forum_bad2 = Forem::Forum.find_or_create_by(
+      :category_id => 3, #Forem::Category.find_by(:name => 'clan-bad2').id,
+      :name => "Bad2 Forum", :description => "Shound not see this")
 
-  post = Forem::Post.find_or_initialize_by(text: "Hello World")
-  post.user = user
+  post1 = Forem::Post.find_or_initialize_by(text: "Hello Clan Forum1")
+  post1.user = user
 
-  topic = Forem::Topic.find_or_initialize_by(subject: "Welcome to Forem")
-  topic.forum = forum
-  topic.user = user
-  topic.posts = [ post ]
+  topic1 = Forem::Topic.find_or_initialize_by(subject: "Welcome to Clan Forum1")
+  topic1.forum = forum1
+  topic1.user = user
+  topic1.posts = [ post1 ]
+  topic1.save!
 
-  topic.save!
+  postb1 = Forem::Post.find_or_initialize_by(text: "Hello Bad Forum1 :(")
+  postb1.user = user
+
+  topicb1 = Forem::Topic.find_or_initialize_by(subject: "Welcome to Bad Forum1 :(")
+  topicb1.forum = forum_bad1
+  topicb1.user = user
+  topicb1.posts = [ postb1 ]
+  topicb1.save!
 end
