9cd8443a43a93e66fff2ea003aba4b879c0bf2fa
Dmytro Lytovchenko <kvakvs@yandex.ru>
2014-09-28 02:16:40 +0200

    saving form fields for reply

--- clan_frontend/app/controllers/post_controller.rb
+++ clan_frontend/app/controllers/post_controller.rb
@@ -4,12 +4,14 @@ class PostController < ApplicationController
   include PostHelper
 
   def new
-    @vars = {}
     # TODO: Check base_title/body access rights
     # Check that 1) User belongs to clan owning the forum
     # or 2) User belongs to allied clan AND forum is public for alliance
-    @vars[:base_title] = @vars[:base_body] = ''
-    unless params[:reply_to].nil?
+    @vars = {}
+    @vars[:base_title] = params[:title] # если вдруг непустой то запишем
+    @vars[:base_body]  = params[:body]  # если вдруг непустой то запишем
+
+    unless params[:reply_to].nil? and not params[:body]
       base_post = PostModel.read_one(session[:user_clan],
                                      params[:thread_id],
                                      params[:reply_to])
@@ -23,7 +25,8 @@ class PostController < ApplicationController
   def create
     limit = 5
     if params[:body].length < limit
-      return redirect_to new_post_path,
+      return redirect_to new_post_path(:body => params[:body],
+                                       :title => params[:title]),3
                          :alert => t('app.forums.body_empty', :limit => limit)
     end
     post_fields = {:body => params[:body],
